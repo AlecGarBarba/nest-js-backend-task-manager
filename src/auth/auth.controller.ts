dcf87e809d614b285895e3949bbddaf711cd2a97
unknown <alecbarba@gmail.com>
2021-09-15 10:39:13 -0500

    auth strategy via jwt implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,7 +6,14 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('/signup')
-  createUser(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
   }
 }
